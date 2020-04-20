85db7fe21497daa62154d974716b595228047fbf
username <user@email.mail>
2020-04-20 15:35:28 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -19,33 +19,23 @@ const styles = theme => ({
     minWidth: 1080
   }
 })
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'ddd',
-  'birthday': '96666',
-  'gender': '남',
-  'job':'학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': 'www',
-  'birthday': '9622566',
-  'gender': '남',
-  'job':'복학'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'dasdd',
-  'birthday': '961516',
-  'gender': '남',
-  'job':'교수'
-}
-]
+
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -62,7 +52,8 @@ class App extends Component {
          </TableRow>
        </TableHead>
        <TableBody>
-      {customers.map(c => { return ( <Customer   key={c.id} id={c.id} image={c.image}name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);}) }
+      {this.state.customers ? this.state.customers.map(c => { 
+        return ( <Customer   key={c.id} id={c.id} image={c.image}name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);}): "" }
       </TableBody>
       </Table>
    </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send( [
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': 'ddd',
+        'birthday': '96666',
+        'gender': '남',
+        'job':'학생'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': 'www',
+        'birthday': '9622566',
+        'gender': '남',
+        'job':'복학'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': 'dasdd',
+        'birthday': '961516',
+        'gender': '남',
+        'job':'교수'
+      }
+    
+        
+    ]);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
